61f6cf8ef9f876ffc1d90fe30833f3af35fd8372
Victoria Fuselier <vic.fuselier@gmail.com>
2024-05-20 22:12:07 -0500

    Attempting to fix button issue
    Both buttons were displaying (and active even if not visible) on live site

--- card.js
+++ card.js
@@ -1,9 +1,13 @@
 const flipBtns = document.getElementsByClassName('flip-btn');
 const card = document.querySelector('.card-inner');
+const frontFlipBtn = document.querySelector('.front-flip-btn');
+const backFlipBtn = document.querySelector('.back-flip-btn');
 
 for (let i=0; i<flipBtns.length; i++) {
     flipBtns[i].addEventListener('click', function() {
         card.classList.toggle('isFlipped');
+        frontFlipBtn.classList.toggle('hidden');
+        backFlipBtn.classList.toggle('hidden');
     });
 }
 
