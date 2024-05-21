ef4b83470c8b11f5d2de7fca7ef0867b2ccc17cc
Victoria Fuselier <vic.fuselier@gmail.com>
2024-05-20 19:32:48 -0500

    Attempting to fix button display on live mobile view
    Both buttons are displaying on live site, BUT only on mobile view. Will make debugging a little challenging
    
    I also reduced font-size on front side of card so it displays one element per line on mobile>>desktop version
    
    I also need to find out what's causing photo to overflow box on landscape (again only on live mobile site)

--- card.js
+++ card.js
@@ -1,7 +1,6 @@
 const flipBtns = document.getElementsByClassName('flip-btn');
 const card = document.querySelector('.card-inner');
 
-
 for (let i=0; i<flipBtns.length; i++) {
     flipBtns[i].addEventListener('click', function() {
         card.classList.toggle('isFlipped');
