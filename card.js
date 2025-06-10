2b7acc2c464a31ecf2004150965e4b914cfc6a8f
Victoria Fuselier <vic.fuselier@gmail.com>
2025-06-10 00:38:53 -0500

    Added some JS rules for accessibility
    Keyboard navigation wasn't working because I hadn't actually made anything on the back-facing side of the card inactive or taken out of display--it was only visually hidden. Also needed aria attributes for screen readers

--- card.js
+++ card.js
@@ -1,15 +1,22 @@
-const flipBtns = document.getElementsByClassName('flip-btn');
+const flipBtns = document.querySelectorAll('.flip-btn');
 const card = document.querySelector('.card-inner');
 const frontFlipBtn = document.querySelector('.front-flip-btn');
 const backFlipBtn = document.querySelector('.back-flip-btn');
-
-for (let i=0; i<flipBtns.length; i++) {
-    flipBtns[i].addEventListener('click', function() {
+const frontFace = document.getElementById('front-face');
+const backFace = document.getElementById('back-face');
+backFace.inert = true;
+  
+flipBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
         card.classList.toggle('isFlipped');
-        setTimeout(()=> {
-            frontFlipBtn.classList.toggle('hidden');
-            backFlipBtn.classList.toggle('hidden');
+        const isFlipped = card.classList.contains('isFlipped');
+        setTimeout(() => {
+            frontFlipBtn.classList.toggle('hidden', isFlipped);
+            backFlipBtn.classList.toggle('hidden', !isFlipped);
+            frontFace.inert = isFlipped;
+            backFace.inert = !isFlipped;
+            frontFace.setAttribute('aria-hidden', isFlipped);
+            backFace.setAttribute('aria-hidden', !isFlipped);
         }, 500);
-    });
-}
-
+    })
+});
